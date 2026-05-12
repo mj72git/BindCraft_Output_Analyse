e5ba0d2fdd1c59b71f863d6cf7c186e67d509c7c
Mohammadjavad(MJ) Shadfar <94294372+mj72git@users.noreply.github.com>
2026-05-12 14:17:51 +1200

    Update app.py

--- app.py
+++ app.py
@@ -312,9 +312,9 @@ if st.session_state.analysis_done and st.session_state.df_out is not None:
                             )
 
                     if show_sphere:
-                        target_sphere_color = st.selectbox("Target Sphere Color", ["limegreenCarbon", "redCarbon", "orangeCarbon", "magentaCarbon", "yellowCarbon", "cyanCarbon"],
+                        target_sphere_color = st.selectbox("Target Sphere Color", ["deepskyblueCarbon", "redCarbon", "orangeCarbon", "magentaCarbon", "yellowCarbon", "cyanCarbon", "spectrum"],
                                             index=0, key=f"tscolor_{selected_design}")
-                        binder_sphere_color = st.selectbox("binder Sphere Color", ["deepskyblueCarbon", "redCarbon", "orangeCarbon", "magentaCarbon", "yellowCarbon", "cyanCarbon"],
+                        binder_sphere_color = st.selectbox("binder Sphere Color", ["limegreenCarbon", "redCarbon", "orangeCarbon", "magentaCarbon", "yellowCarbon", "cyanCarbon", "spectrum"],
                                             index=0, key=f"bscolor_{selected_design}")
                         for chain, resi in highlight_target:
                             view.addStyle(
